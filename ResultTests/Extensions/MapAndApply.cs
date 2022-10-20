0633b71a47be66bbc7d27fc89fc85725b9359a23
jhecht <j.hecht@it-p.de>
2022-10-20 11:13:50 +0200

    added tests

--- ResultTests/Extensions/MapAndApply.cs
+++ ResultTests/Extensions/MapAndApply.cs
@@ -179,6 +179,32 @@ public class MapAndApply
         }
 
         [Fact]
+        public void Third_fails()
+        {
+            // arrange
+            var value1 = Result<string>.Success("input1");
+            var value2 = Result<string>.Success("input2");
+            var value3 = Result<string>.Failure("error");
+            var testFunc = (string a, string b, string c) => a + b + c;
+
+            // act
+            var failure = value1
+                .Map(testFunc)
+                .Apply(value2)
+                .Apply(value3);
+
+
+            // assert
+            var error = failure.GetError();
+            using (new AssertionScope())
+            {
+                failure.IsSuccess.Should().BeFalse();
+                error.Should().NotBeNull();
+                error!.Message.Should().Be("error");
+            }
+        }
+
+        [Fact]
         public void All_three_fail()
         {
             // arrange
@@ -202,6 +228,175 @@ public class MapAndApply
                 error.Should().NotBeNull();
                 error!.Message.Should().Be("There are 3 Errors. ");
                 error!.Display.Should().Be("CompositeError: [TextError: error1, TextError: error2, TextError: error3]");
+            }
+        }
+    }
+    public class Apply_four_params
+    {
+        [Fact]
+        public void Success()
+        {
+            // arrange
+            var value1 = Result<string>.Success("input1");
+            var value2 = Result<string>.Success("input2");
+            var value3 = Result<string>.Success("input3");
+            var value4 = Result<string>.Success("input4");
+            var testFunc = (string a, string b, string c, string d) => a + b + c + d;
+
+            // act
+            var success = value1
+                .Map(testFunc)
+                .Apply(value2)
+                .Apply(value3)
+                .Apply(value4);
+
+
+            // assert
+            var value = success.GetValue();
+            using (new AssertionScope())
+            {
+                success.IsSuccess.Should().BeTrue();
+                value.Should().Be("input1input2input3input4");
+            }
+        }
+
+        [Fact]
+        public void First_fails()
+        {
+            // arrange
+            var value1 = Result<string>.Failure("error");
+            var value2 = Result<string>.Success("input2");
+            var value3 = Result<string>.Success("input3");
+            var value4 = Result<string>.Success("input4");
+            var testFunc = (string a, string b, string c, string d) => a + b + c + d;
+
+            // act
+            var failure = value1
+                .Map(testFunc)
+                .Apply(value2)
+                .Apply(value3)
+                .Apply(value4);
+
+
+            // assert
+            var error = failure.GetError();
+            using (new AssertionScope())
+            {
+                failure.IsSuccess.Should().BeFalse();
+                error.Should().NotBeNull();
+                error!.Message.Should().Be("error");
+            }
+        }
+
+        [Fact]
+        public void Second_fails()
+        {
+            // arrange
+            var value1 = Result<string>.Success("input1");
+            var value2 = Result<string>.Failure("error");
+            var value3 = Result<string>.Success("input3");
+            var value4 = Result<string>.Success("input4");
+            var testFunc = (string a, string b, string c, string d) => a + b + c + d;
+
+            // act
+            var failure = value1
+                .Map(testFunc)
+                .Apply(value2)
+                .Apply(value3)
+                .Apply(value4);
+
+
+            // assert
+            var error = failure.GetError();
+            using (new AssertionScope())
+            {
+                failure.IsSuccess.Should().BeFalse();
+                error.Should().NotBeNull();
+                error!.Message.Should().Be("error");
+            }
+        }
+
+        [Fact]
+        public void Third_fails()
+        {
+            // arrange
+            var value1 = Result<string>.Success("input1");
+            var value2 = Result<string>.Success("input2");
+            var value3 = Result<string>.Failure("error");
+            var value4 = Result<string>.Success("input4");
+            var testFunc = (string a, string b, string c, string d) => a + b + c + d;
+
+            // act
+            var failure = value1
+                .Map(testFunc)
+                .Apply(value2)
+                .Apply(value3)
+                .Apply(value4);
+
+
+            // assert
+            var error = failure.GetError();
+            using (new AssertionScope())
+            {
+                failure.IsSuccess.Should().BeFalse();
+                error.Should().NotBeNull();
+                error!.Message.Should().Be("error");
+            }
+        }
+        [Fact]
+        public void Fourth_fails()
+        {
+            // arrange
+            var value1 = Result<string>.Success("input1");
+            var value2 = Result<string>.Success("input2");
+            var value3 = Result<string>.Success("input3");
+            var value4 = Result<string>.Failure("error");
+            var testFunc = (string a, string b, string c, string d) => a + b + c + d;
+
+            // act
+            var failure = value1
+                .Map(testFunc)
+                .Apply(value2)
+                .Apply(value3)
+                .Apply(value4);
+
+
+            // assert
+            var error = failure.GetError();
+            using (new AssertionScope())
+            {
+                failure.IsSuccess.Should().BeFalse();
+                error.Should().NotBeNull();
+                error!.Message.Should().Be("error");
+            }
+        }
+
+        [Fact]
+        public void All_four_fail()
+        {
+            // arrange
+            var value1 = Result<string>.Failure("error1");
+            var value2 = Result<string>.Failure("error2");
+            var value3 = Result<string>.Failure("error3");
+            var value4 = Result<string>.Failure("error4");
+            var testFunc = (string a, string b, string c, string d) => a + b + c + d;
+
+            // act
+            var failure = value1
+                .Map(testFunc)
+                .Apply(value2)
+                .Apply(value3)
+                .Apply(value4);
+
+
+            // assert
+            var error = failure.GetError();
+            using (new AssertionScope())
+            {
+                failure.IsSuccess.Should().BeFalse();
+                error.Should().NotBeNull();
+                error!.Message.Should().Be("There are 4 Errors. ");
+                error!.Display.Should().Be("CompositeError: [TextError: error1, TextError: error2, TextError: error3, TextError: error4]");
             }
         }
     }
