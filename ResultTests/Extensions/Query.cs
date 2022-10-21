7914655e1ee9b9142f79c29ee51fd83ba9af4f22
jhecht <j.hecht@it-p.de>
2022-10-21 19:55:44 +0200

    krasse select manies !!!

--- ResultTests/Extensions/Query.cs
+++ ResultTests/Extensions/Query.cs
@@ -4,46 +4,145 @@ namespace ResultTests.Extensions;
 
 public sealed class Query
 {
-    [Fact]
-    public void Success()
+    public sealed class Sync
     {
-        // arrange + act
-        var result =
-            from a in Result<string>.Success("first")
-            from b in Result<string>.Success("second")
-            from c in Result<string>.Success("third")
-            from d in Result<string>.Success("fourth")
-            from e in Result<string>.Success("fifth")
-            select $"{a} {b} {c} {d} {e}";
-
-        // assert
-        var value = result.GetValue();
-        using (new AssertionScope())
+        [Fact]
+        public void Success()
         {
-            result.IsSuccess.Should().BeTrue();
-            value.Should().NotBeNull();
-            value.Should().Be("first second third fourth fifth");
+            // arrange + act
+            var result =
+                from a in Result<string>.Success("first")
+                from b in Result<string>.Success("second")
+                from c in Result<string>.Success("third")
+                from d in Result<string>.Success("fourth")
+                from e in Result<string>.Success("fifth")
+                select $"{a} {b} {c} {d} {e}";
+
+            // assert
+            var value = result.GetValue();
+            using (new AssertionScope())
+            {
+                result.IsSuccess.Should().BeTrue();
+                value.Should().NotBeNull();
+                value.Should().Be("first second third fourth fifth");
+            }
+        }
+
+        [Fact]
+        public void Failure()
+        {
+            // arrange + act
+            var result =
+                from a in Result<string>.Success("first")
+                from b in Result<string>.Success("second")
+                from c in Result<string>.Failure("third")
+                from d in Result<string>.Success("fourth")
+                from e in Result<string>.Success("fifth")
+                select $"{a} {b} {c} {d} {e}";
+
+            // assert
+            var error = result.GetError();
+            using (new AssertionScope())
+            {
+                result.IsSuccess.Should().BeFalse();
+                error.Should().NotBeNull();
+                error!.Message.Should().Be("third");
+            }
         }
     }
-    [Fact]
-    public void Failure()
-    {
-        // arrange + act
-        var result =
-            from a in Result<string>.Success("first")
-            from b in Result<string>.Success("second")
-            from c in Result<string>.Failure("third")
-            from d in Result<string>.Success("fourth")
-            from e in Result<string>.Success("fifth")
-            select $"{a} {b} {c} {d} {e}";
 
-        // assert
-        var error = result.GetError();
-        using (new AssertionScope())
+    public sealed class Async
+    {
+        [Fact]
+        public async Task Success()
         {
-            result.IsSuccess.Should().BeFalse();
-            error.Should().NotBeNull();
-            error!.Message.Should().Be("third");
+            // arrange + act
+            var result = await
+            (
+                from a in Result<string>.Success("first").ToTask()
+                from b in Result<string>.Success("second").ToTask()
+                from c in Result<string>.Success("third").ToTask()
+                from d in Result<string>.Success("fourth").ToTask()
+                from e in Result<string>.Success("fifth").ToTask()
+                select $"{a} {b} {c} {d} {e}");
+
+            // assert
+            var value = result.GetValue();
+            using (new AssertionScope())
+            {
+                result.IsSuccess.Should().BeTrue();
+                value.Should().NotBeNull();
+                value.Should().Be("first second third fourth fifth");
+            }
+        }
+
+        [Fact]
+        public async Task Failure()
+        {
+            // arrange + act
+            var result = await
+            (
+                from a in Result<string>.Success("first").ToTask()
+                from b in Result<string>.Success("second").ToTask()
+                from c in Result<string>.Failure("third").ToTask()
+                from d in Result<string>.Success("fourth").ToTask()
+                from e in Result<string>.Success("fifth").ToTask()
+                select $"{a} {b} {c} {d} {e}");
+
+            // assert
+            var error = result.GetError();
+            using (new AssertionScope())
+            {
+                result.IsSuccess.Should().BeFalse();
+                error.Should().NotBeNull();
+                error!.Message.Should().Be("third");
+            }
+        }
+
+        [Fact]
+        public async Task Mixed_Success()
+        {
+            // arrange + act
+            var result = await
+            (
+                from a in Result<string>.Success("first")
+                from b in Result<string>.Success("second").ToTask()
+                from c in Result<string>.Success("third")
+                from d in Result<string>.Success("fourth").ToTask()
+                from e in Result<string>.Success("fifth").ToTask()
+                select $"{a} {b} {c} {d} {e}");
+
+            // assert
+            var value = result.GetValue();
+            using (new AssertionScope())
+            {
+                result.IsSuccess.Should().BeTrue();
+                value.Should().NotBeNull();
+                value.Should().Be("first second third fourth fifth");
+            }
+        }
+
+        [Fact]
+        public async Task Mixed_Failure()
+        {
+            // arrange + act
+            var result = await
+            (
+                from a in Result<string>.Success("first").ToTask()
+                from b in Result<string>.Success("second")
+                from c in Result<string>.Failure("third").ToTask()
+                from d in Result<string>.Success("fourth").ToTask()
+                from e in Result<string>.Success("fifth")
+                select $"{a} {b} {c} {d} {e}");
+
+            // assert
+            var error = result.GetError();
+            using (new AssertionScope())
+            {
+                result.IsSuccess.Should().BeFalse();
+                error.Should().NotBeNull();
+                error!.Message.Should().Be("third");
+            }
         }
     }
 }
--- Tool/Program.cs
+++ Tool/Program.cs
@@ -1,10 +1,16 @@
 ﻿using Results;
 
-
-var asd =
-    from a in Result<string>.Success("hallo")
-    from b in Result<string>.Failure("hallo")
-    from c in Result<string>.Success("hallo")
-    select $"{a} + {b} + {c}";
+var qwe = Task.Run(async () =>
+{
+    await Task.Delay(10_000);
+    return Result<string>.Success("spät");
+});
+var asd = await (
+    from a in Result<string>.Success("hallo").ToTask()
+    from b in Result<string>.Success("hallo")
+    from c in Result<string>.Success("hallo").ToTask()
+    from d in qwe
+    select $"{a} + {b} + {c} # {d}");
 
 asd.Tap(Console.WriteLine, e => Console.WriteLine(e.Display));
+Console.ReadKey();
